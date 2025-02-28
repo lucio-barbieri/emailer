6ce03bf29d01c3347c0e28ed31af6664fe095abe
lucio <lucio.barbieri@bqn.com.uy>
2025-02-27 23:15:58 -0300

    Append recipient name to email content. Updated README.

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # emailer
 
-Script to send personalized email (with attachments) to several recipients.
+Script to send a personalized email (with attachments) to several recipients.
 
 Usage:
 ```bash
 python3 emailer.py
 ```
+
+## email data
+The email configuration file.
+
+Example:
+```text
+Source_addr: the-sending-email-address@example.com
+Password: password
+Subject: example-subject
+Attachment_paths: first-file.pdf, second-file.pdf, ...
+Salute: your-salute
+Content: the-content-of-the-email
+new-lines are possible!
+```
+
+## email list
+Excel file with the name of the recipient in the first column, and the email address in the second column.
+
+## TODO list:
+
+* Make configuration file a JSON file.
+* Speed up the email-sending process
--- emailer.py
+++ emailer.py
@@ -12,7 +12,7 @@ def get_names_and_emails_from_excel(p_filename):
 
     names = []
     emails = []
-    for row in range(1, active.max_row): # First row is header data
+    for row in range(active.max_row):
         name = active[name_col][row].value
         email = active[email_col][row].value
         if (email != None):
@@ -25,6 +25,13 @@ def get_names_and_emails_from_excel(p_filename):
 
 def get_email_data(p_filename):
     with open(p_filename, 'r') as f:
+        subject = ""
+        src_email = ""
+        src_pass = ""
+        salute = ""
+        content = ""
+        attachment_paths = []
+
         for line in f.readlines():
             if line.startswith('Source_addr: '):
                 src_email = line[len('Source_addr: '):]
@@ -32,25 +39,30 @@ def get_email_data(p_filename):
                 src_pass = line[len('Password: '):]
             if line.startswith('Subject: '):
                 subject = line[len('Subject: '):]
-            if line.startswith('Attachment_paths: '):
-                attachment_paths = line[len('Attachment_paths: '):]
+            if line.startswith('Salute: '):
+                salute = line[len('Salute: '):]
             if line.startswith('Content: '):
                 content = line[len('Content: '):]
+            if line.startswith('Attachment_paths: '):
+                attachment_paths = line[len('Attachment_paths: '):]
 
         src_email = src_email.split('\n')[0]
         src_pass = src_pass.split('\n')[0]
         subject = subject.split('\n')[0]
-        attachment_paths = attachment_paths.split('\n')[0].replace(' ','').split(',')
+        salute = salute.split('\n')[0]
         content = content[:-1]
+        attachment_paths = attachment_paths.split('\n')[0].replace(' ','').split(',')
 
-    return subject, src_email, src_pass, content, attachment_paths 
+    return subject, src_email, src_pass, salute, content, attachment_paths 
 
-def append_name_to_content(p_dst_name, p_content):
-    # todo
-    return p_content
+def append_name_to_content(p_dst_name, p_salute, p_content):
+    if not p_salute.endswith(' '):
+        p_salute = p_salute + " "
+    updated_content = p_salute + p_dst_name + ",\n" + p_content
+    return updated_content
 
 names, emails = get_names_and_emails_from_excel('email_list.xlsx')
-subject, src_email, src_pass, content, attachment_paths = get_email_data("email_data.txt")
+subject, src_email, src_pass, salute, content, attachment_paths = get_email_data("email_data.txt")
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
     print("Server opened ...")
@@ -62,8 +74,8 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
         msg['Subject'] = subject
         msg['From'] = src_email
         msg['To'] = dst_email
-        content = append_name_to_content(dst_name, content)
-        msg.set_content(content)
+        updated_content = append_name_to_content(dst_name, salute, content)
+        msg.set_content(updated_content)
 
         for attc in attachment_paths:
             with open(attc, 'rb') as f:
